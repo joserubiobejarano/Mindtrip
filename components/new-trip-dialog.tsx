b85a4ad93cdc51ba4864a260b0c7044555395a8d
Jose Rubio <joserubiobejarano@gmail.com>
2025-12-13 19:19:56 +0100

    new  stuff

--- components/new-trip-dialog.tsx
+++ components/new-trip-dialog.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
